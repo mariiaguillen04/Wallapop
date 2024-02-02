a7f7e4961b66dace58e652e0ce278af082aeb25d
Humanito04 <149381198+Humanito04@users.noreply.github.com>
2024-02-02 12:52:59 +0100

    Update Producto.java

--- wallapop/compraventa/Producto.java
+++ wallapop/compraventa/Producto.java
@@ -1,5 +1,66 @@
 package wallapop.compraventa;
 
 public class Producto {
+  // Atributos
+	private static int codigo;
+	private String nombre;
+	private double precio;
+	private EstadoProducto estado;
+	
+	// Constructores
+	public Producto(String nombre,  String estado, int codigo) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = 0.0;
+		this.estado = EstadoProducto.valueOf(estado);
+		
+	}
+	
+	
+	public Producto(String nombre,  String estado, double precio) {
+		this.codigo = 1;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.estado = EstadoProducto.valueOf(estado);
+	}
 
+
+	public static int getCodigo() {
+		return codigo;
+	}
+
+
+	public static void setCodigo(int codigo) {
+		Producto.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public double getPrecio() {
+		return this.precio;
+	}
+
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+
+	public EstadoProducto getEstado() {
+		return this.estado;
+	}
+
+
+	public void setEstado(String estado) {
+		this.estado = EstadoProducto.valueOf(estado);
+	}
 }
